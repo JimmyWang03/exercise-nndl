d566995cf6ff276f7d1e9bbf64b147de25f93541
907229050@qq.com <907229050@qq.com>
2024-03-27 00:14:09 +0800

    finish

--- chap6_RNN/tangshi_for_pytorch/main.py
+++ chap6_RNN/tangshi_for_pytorch/main.py
@@ -222,12 +222,12 @@ def gen_poem(begin_word):
 
 
 pretty_print_poem(gen_poem("日"))
-pretty_print_poem(gen_poem("红"))
-pretty_print_poem(gen_poem("山"))
-pretty_print_poem(gen_poem("夜"))
-pretty_print_poem(gen_poem("湖"))
-pretty_print_poem(gen_poem("湖"))
-pretty_print_poem(gen_poem("湖"))
-pretty_print_poem(gen_poem("君"))
+# pretty_print_poem(gen_poem("红"))
+# pretty_print_poem(gen_poem("山"))
+# pretty_print_poem(gen_poem("夜"))
+# pretty_print_poem(gen_poem("湖"))
+# pretty_print_poem(gen_poem("海"))
+# pretty_print_poem(gen_poem("月"))
+
 
 
